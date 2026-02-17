5d5eee10d78df591bb70928fcfadff6ef435b8c3
shashikant kumar <sp8326441@gmail.com>
2026-02-17 20:28:06 +0530

    Add pandas script to extract marks column as Series

---
+++ DataFrame & series/student_marks_series.py
@@ -0,0 +1,13 @@
+import pandas as pd
+student = {
+    "name" : ['sam','tom','stark','ram','shyam'],
+    "age" : [21,22,21,20,20],
+    "marks" : [88,87,78,98,92],
+    "city" : ['mumbai','delhi','mumbai','patna','noida']
+}
+df = pd.DataFrame(student)
+print(df)
+
+marks_series = df["marks"]
+print("\nmarks series")
+print(marks_series)
