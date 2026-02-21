4e27c0dcf638af34ee6e32cb0c0293e7db6e335b
shashikant kumar <sp8326441@gmail.com>
2026-02-21 17:12:09 +0530

    Add employee DataFrame structure inspection script

---
+++ DataFrame & series/employee_dataframe_structure.py
@@ -0,0 +1,13 @@
+import pandas as pd
+data = {
+    "Name": ["Rahul", "Aman", "Priya", "Neha", "Rohit", "Kajal", "Vikas", "Sneha"],
+    "Age": [25, 28, 24, 27, 30, 26, 29, 23],
+    "Salary": [45000, 52000, 48000, 50000, 60000, 47000, 55000, 43000],
+    "Performance_Scor-e": [8.5, 7.2, 9.1, 6.8, 8.0, 7.5, 8.7, 6.9]
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+print("shape:",df.shape)
+print("column name:",df.columns)
