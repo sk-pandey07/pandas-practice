ac976f0d7e0869ea0ca6459adae14a896570648a
shashikant kumar <sp8326441@gmail.com>
2026-02-22 22:59:06 +0530

    Create 02.py

---
+++ practice set part 1/02.py
@@ -0,0 +1,22 @@
+import pandas as pd
+data = {
+    "Name": ["Amit", "Rahul", "Sneha", "Priya", "Arjun",
+             "Neha", "Vikash", "Pooja", "Karan", "Anjali",
+             "Rohan", "Meera", "Saurabh", "Kavita", "Deepak"],
+    
+    "Age": [24, 28, 26, 30, 27,
+            25, 29, 31, 26, 28,
+            27, 32, 29, 30, 26],
+    
+    "Department": ["IT", "HR", "Finance", "IT", "Marketing",
+                   "HR", "IT", "Finance", "Marketing", "IT",
+                   "HR", "Finance", "IT", "Marketing", "HR"],
+    
+    "Salary": [45000, 52000, 48000, 60000, 50000,
+               47000, 75000, 68000, 54000, 72000,
+               51000, 80000, 62000, 59000, 53000]
+}
+
+df = pd.DataFrame(data)
+print("\noriginal dataset")
+print(df)
