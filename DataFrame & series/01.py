203d09272835be36a661b448fd8769bbe689c6ad
shashikant kumar <sp8326441@gmail.com>
2026-02-26 19:54:04 +0530

    Create 01.py

---
+++ DataFrame & series/01.py
@@ -0,0 +1,15 @@
+import pandas as pd
+data = {
+    "Name": ["Aman", "Ria", "Shyam"],
+    "Age": [21, 19, 23],
+    "Score": [88, 92, 77]
+}
+
+df = pd.DataFrame(data)
+print(df)
+# display first two rows
+print(df.head(2))
+
+# print(df["Name"])
+
+print(df[["Name"]])
