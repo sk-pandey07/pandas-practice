1e5b8d764b6eac0f493ea87353579d71f3550146
shashikant kumar <sp8326441@gmail.com>
2026-03-02 19:23:47 +0530

    Create 01.py

---
+++ level 2/01.py
@@ -0,0 +1,25 @@
+import pandas as pd
+data = {
+    "ID": range(1, 16),
+    "Name": ["Aman", "Riya", "Shyam", "Neha", "Rahul",
+             "Priya", "Karan", "Sneha", "Vikas", "Anjali",
+             "Rohit", "Pooja", "Arjun", "Meera", "Sahil"],
+    
+    "Age": [20, 19, 21, 20, 22,
+            19, 21, 20, 23, 22,
+            21, 20, 19, 22, 23],
+    
+    "Maths": [85, 78, 90, 88, 76,
+              95, 67, 80, 72, 89,
+              91, 73, 84, 77, 69],
+    
+    "English": [75, 82, 88, 79, 85,
+                90, 70, 76, 80, 87,
+                92, 74, 81, 78, 73],
+    
+    "Computer": [92, 85, 94, 90, 88,
+                 96, 72, 84, 79, 91,
+                 95, 77, 89, 83, 75]
+}
+df = pd.DataFrame(data)
+print(df)
